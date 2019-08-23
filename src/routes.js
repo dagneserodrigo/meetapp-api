499439532c852256398d689c7e0b3903828bf067
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2019-08-23 17:36:45 -0300

    feat(UserController): store user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
   res.json({ message: 'home' });
 });
+
+routes.post('/users', UserController.store);
 
 export default routes;
